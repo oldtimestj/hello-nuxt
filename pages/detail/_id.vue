7631362c6f0cc5070d5631b616ecce3ac7cbf7f2
oldtimestj <85237486@qq.com>
2020-08-16 15:57:58 +0800

    异步请求数据

---
+++ pages/detail/_id.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>详情页</h2>
+    {{$route.params.id}}
+    <p>{{detail.price}}</p>
+    <p>{{detail.description}}</p>
+  </div>
+</template>
+<script>
+  import axios from 'axios';
+  export default {
+    async asyncData() {
+     const res = await axios.get('/data/test.json');
+     console.log('res', res)
+      return {
+          detail: res.data,
+        }
+    }
+  }
+</script>
